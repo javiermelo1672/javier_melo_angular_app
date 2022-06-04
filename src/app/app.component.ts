a69faf6382b020f0f90a44f7345d1c5bd1368010
Javier Melo <javier.hospital@sofka.com.co>
2022-06-04 15:30:07 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'javier_melo_angular_app';
+}
